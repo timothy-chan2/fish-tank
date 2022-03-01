2fa99ab1a7333506dffb83c192e84c2b0918c0d0
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-03-01 01:19:40 +0000

    Enlarge the BiteFish when you click on it

--- public/scripts/entities/bite_fish.js
+++ public/scripts/entities/bite_fish.js
@@ -4,4 +4,9 @@ class BiteFish extends Fish {
     super(options); // Call super to run the code inside `Fish`'s constructor
     this.imageUri = '/images/bite_fish.gif'; // Set the image
   }
+
+  onClick(event) {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
 }
